d00880d3dfbb39fddebc3adba1c4cf302e255063
gauravjoy12 <gauravjoy12@gmail.com>
2024-04-24 23:04:35 +0530

    Macro is working

---
+++ Excecute_macro.py
@@ -0,0 +1,26 @@
+import xlwings as xw
+workbook_path = "C:\\Users\\X250380\\OneDrive - MerckGroup\\Desktop\\SoPo Macro\\Macro_file_1.xlsm"
+macro_name ="MainZCBB.MainZCBB"
+macro_name2 ="mainZCOR.ZCOR"
+try:
+
+        # Start an Excel application
+        app = xw.App(visible=False)  # visible=False hides the Excel application window
+
+        # Open the workbook
+        workbook = app.books.open(workbook_path)
+
+        # Run the macro
+        workbook.macro(macro_name)()
+        workbook.macro(macro_name2)()
+
+
+        # Save and close the workbook
+        workbook.save()
+        workbook.close()
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+    # Quit the Excel application
+    app.quit()
---
+++ Read Excel.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# Define the workbook path and the output folder
+workbook_path = "C:\\Users\\X250380\\OneDrive - MerckGroup\\Desktop\\SoPo Macro\\Macro_file_1.xlsm"
+output_folder = "C:\\Users\\X250380\\OneDrive - MerckGroup\\Desktop\\SoPo Macro\\\Output File\\"
+Material_Code="CVGL71TP3"
+
+# Define the file names of the reports you want to read
+file1 = output_folder + "Final_Reports_ZCBB.xlsx"
+file2 = output_folder + "Final_Reports_ZCOR.xlsx"
+
+df1 = pd.read_excel(file1)
+df2 = pd.read_excel(file2)
+combined_df = pd.concat([df1, df2])
+#filtered_df1 = df1['Material_Code'] == material_code]
+rslt_df = df1.loc[df1['Material Code'] == Material_Code ]
+rslt_df2 = df2.loc[df2['Material Code'] == Material_Code ]
+
+
+
+
+print(combined_df.head(10))
+print(rslt_df)
+print(rslt_df2)
+
+
